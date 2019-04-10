211e3878c3856aa6c70a84bba03e8c19c568085e
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-10 17:28:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources only: posts: [:index, :new, :create, :show, :edit, :update]
-  resources only: authors: [:index, :new, :create, :show, :edit, :update]
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update]
+  resources :authors, only: [:index, :new, :create, :show, :edit, :update]
 end
