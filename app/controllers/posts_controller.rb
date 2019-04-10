052ae145a4d17c5218e34ccf1b24f2b5ad3787a3
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-10 17:35:28 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
-    else 
-      render :edit 
+    else
+      render :edit
     end
   end
 
